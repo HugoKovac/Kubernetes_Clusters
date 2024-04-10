0610e1d20adbc6279d19c04a1b73387d6a995b0e
Antoine-dev <aleduc@student.42.fr>
2024-04-10 17:51:22 +0200

    Sed command, dl and update argocd manifest directly

--- part3/scripts/setup.sh
+++ part3/scripts/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Création du cluster Kubernetes avec k3d
-k3d cluster create mon-cluster --api-port 8000
+k3d cluster create dev-cluster --api-port 8000 --port 8080:80@loadbalancer --port 8443:443@loadbalancer --port 8888:8888@loadbalancer
 
 # Création des namespaces
 kubectl create namespace argocd
 kubectl create namespace dev
 
+# Download argocd manifest
+#wget https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# Patch install manifest to add config to argocd
+#sed -i '/- argocd-server/{n;/- --staticassets/{n;/- \/shared\/app/{s/$/\n        - --insecure\n        - --rootpath\n        - \/argocd/;};};}' install.yaml
+
 # Installation d'ArgoCD dans le namespace argocd
+#kubectl apply -n argocd -f install.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
