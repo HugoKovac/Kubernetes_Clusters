d839f251616f60af594e94d6c0c647cd216739e9
Antoine-dev <aleduc@student.42.fr>
2024-04-10 17:18:24 +0200

    Update script

--- part3/scripts/argocd.sh
+++ part3/scripts/argocd.sh
@@ -4,7 +4,7 @@
 kubectl port-forward svc/argocd-server -n argocd 8080:443 >/dev/null 2>&1 &
 
 # Initial password and future password
-ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+ARGOCD_PASSWORD=$(argocd admin initial-password -n argocd | head -n 1)
 PASS="password"
 
 # ArgoCD login
@@ -18,7 +18,18 @@ argocd logout localhost:8080
 argocd login localhost:8080 --username admin --password $PASS --insecure
 
 # ArgoCD app creation
-argocd app create -f wil-application.yaml
+argocd app create -f scripts/wil-application.yaml
 
-# External access point to our pod (curl localhost:8888 after to check)
-kubectl port-forward wil-pod-7dc8bc657c-zfsdz 8888:8888 >/dev/null 2>&1 &
+sleep 45
+
+# Exécutez kubectl get pods et extrayez le nom du pod
+pod_name=$(kubectl get pods -o=name | grep wil-pod)
+
+# Vérifiez si le nom du pod est trouvé
+if [ -n "$pod_name" ]; then
+	# Lancez le port-forwarding avec le nom du pod trouvé
+	kubectl port-forward "$pod_name" 8888:8888 >/dev/null 2>&1 &
+	echo "Port-forwarding démarré vers le pod $pod_name"
+else
+	echo "Aucun pod trouvé avec le nom wil-pod"
+fi
