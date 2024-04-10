d1533e93544e472ef46010f1d09c11a3f37e614f
Hugo Kovac <kovmaxime@gmail.com>
2024-04-10 21:27:12 +0200

    change repo name

--- part3/scripts/setup.sh
+++ part3/scripts/setup.sh
@@ -17,10 +17,37 @@ kubectl create namespace dev
 #kubectl apply -n argocd -f install.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-sleep 5
+sleep 10
 
 echo wait for argo
 kubectl wait --for=condition=Ready pod --all -n argocd --timeout=300s
 
 echo port-foward argo
 kubectl port-forward svc/argocd-server -n argocd 8080:80 >/dev/null 2>&1 &
+
+# External access point to ArgoCD
+kubectl port-forward svc/argocd-server -n argocd 8080:443 >/dev/null 2>&1 &
+
+# Initial password and future password
+ARGOCD_PASSWORD=$(argocd admin initial-password -n argocd | head -n 1)
+PASS="password"
+
+# ArgoCD login
+argocd login localhost:8080 --username admin --password $ARGOCD_PASSWORD --insecure
+
+# Update password
+argocd account update-password --current-password $ARGOCD_PASSWORD --new-password $PASS
+
+# Login with new password
+argocd logout localhost:8080
+argocd login localhost:8080 --username admin --password $PASS --insecure
+
+# ArgoCD app creation
+argocd app create -f scripts/wil-application.yaml
+
+sleep 10
+
+echo wait for wilpod
+kubectl wait --for=condition=Ready pod --all -n dev --timeout=300s
+
+while true; do kubectl port-forward svc/wil-service 8888:8888 -n dev >/dev/null 2>&1 ; done&
